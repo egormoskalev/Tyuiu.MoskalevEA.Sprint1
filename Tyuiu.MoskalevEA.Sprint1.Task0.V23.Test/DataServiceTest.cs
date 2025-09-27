41a4a282dd1bdbfd651e4eeeaece6ec4fd7d7cb3
Егор Москалев <moskalevegor310@gmail.com>
2025-09-27 15:34:40 +0500

    Add project files.

---
+++ Tyuiu.MoskalevEA.Sprint1.Task0.V23.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.MoskalevEA.Sprint1.Task0.V23.Lib;
+
+namespace Tyuiu.MoskalevEA.Sprint1.Task0.V23.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(13, res);
+        }
+    }
+}
---
+++ Tyuiu.MoskalevEA.Sprint1.Task0.V23/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.MoskalevEA.Sprint1.Task0.V23.Lib;
+
+namespace Tyuiu.MoskalevEA.Sprint1.Task0.V23
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Москалев Е. А. | ИСПб-25-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #                                                               *");
+            Console.WriteLine("* Тема: Создания итогового решения по спринту                            *");
+            Console.WriteLine("* Задание #0                                                             *");
+            Console.WriteLine("* Вариант #23                                                            *");
+            Console.WriteLine("* Выполнил:Москалев Егор Александрович. | ИСПб-25-1                      *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение (15/5*4)+1             *");
+            Console.WriteLine("* и печатает результат на экране.                                        *");
+            Console.WriteLine("*                                                                        *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine(ds.Calculate());
+
+            Console.ReadLine();
+        }
+    }
+}
