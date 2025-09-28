d755dcb325d6b506e04b0fb72bfbcc611d0da7cb
Егор Москалев <moskalevegor310@gmail.com>
2025-09-28 23:07:45 +0500

    Task2V3

--- Tyuiu.MoskalevEA.Sprint1.Task2.V3/Program.cs
+++ Tyuiu.MoskalevEA.Sprint1.Task2.V3/Program.cs
@@ -8,14 +8,26 @@ namespace Tyuiu.MoskalevEA.Sprint1.Task2.V3
         {
             DataService ds = new DataService();
 
+            Console.Title = "Спринт #1 | Выполнил: Москалев Е. А. | ИСПб-25-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #                                                               *");
+            Console.WriteLine("* Тема: Создания итогового решения по спринту                            *");
+            Console.WriteLine("* Задание #2                                                             *");
+            Console.WriteLine("* Вариант #3                                                             *");
+            Console.WriteLine("* Выполнил:Москалев Егор Александрович. | ИСПб-25-1                      *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные,*");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.            *");
+            Console.WriteLine("*                                                                        *");
             Console.WriteLine("**************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
             Console.WriteLine("**************************************************************************");
 
-            int x;
+            double x;
 
             Console.WriteLine("Введите значение X:");
-            x = Convert.ToInt32(Console.ReadLine());
+            x = Convert.ToDouble(Console.ReadLine());
 
             Console.WriteLine("**************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
